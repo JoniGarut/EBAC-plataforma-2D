f3aa4321577c21607ba22baf6316d99586fe79ad
JoniGarut <zechs.renato@gmail.com>
2022-09-08 18:59:57 -0300

    atrito / friction correction

--- Assets/Scripts/Player1/Player.cs
+++ Assets/Scripts/Player1/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
 
@@ -24,20 +24,39 @@ public class Player : MonoBehaviour
     private void HandleMovement()
     {
 
+        #region Basic Move X
+
+        // BASIC : x coord moves rigidbody left and right through velocity and keycode input
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
         }
-
         if (Input.GetKey(KeyCode.RightArrow))
         {
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
         }
+
+        //atrito
+        if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+        if (myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+
+        #endregion
     }
 
+    #region JUMP Y
+
+    // BASIC : y coord moves through vector2.UP multiplied by forceJump reference in unity and keycode input 
     private void HandleJump()
     {
         if (Input.GetKey(KeyCode.Space))
             myRigidbody.velocity = Vector2.up * forceJump;
     }
+
+    #endregion
 }
