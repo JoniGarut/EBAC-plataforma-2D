1040972a1891bafc7b8e421a14902b4d4ca44675
JoniGarut <zechs.renato@gmail.com>
2022-09-08 19:21:56 -0300

    last commit late updates

--- Assets/Scripts/Player1/Player.cs
+++ Assets/Scripts/Player1/Player.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("P1 References")]
+
+    #region P1 References
+
     public Rigidbody2D myRigidbody;
 
     public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
+    public float speedRun;
+
+    private float _currentSpeed;
 
     public float forceJump = 50f;
+
+    #endregion
 
 
     // Update is called once per frame
@@ -26,15 +35,22 @@ public class Player : MonoBehaviour
 
         #region Basic Move X
 
+
+        // RUN!!!!
+        if (Input.GetKey(KeyCode.LeftControl))
+            _currentSpeed = speedRun;
+        else
+            _currentSpeed = speed;
+
         // BASIC : x coord moves rigidbody left and right through velocity and keycode input
-        if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.LeftArrow))
         {
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
-        }
+            myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
+        }       
 
         //atrito
         if (myRigidbody.velocity.x < 0)
